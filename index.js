fedcf5b7918bc95b82970f3bb24f95b576074e8c
ErikPinel <erik.zusin@gmail.com>
2024-02-25 02:14:33 +0200

    server

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ mongoose
     autoIndex: true,
   })
   .then(() => {
-    server.listen('https://lazy-pink-fawn-hose.cyclic.app', () => {
+    server.listen('http://lazy-pink-fawn-hose.cyclic.app', () => {
       console.log("SERVER IS RUNNING");
     });
   });
